317c8f245cd0ca842c1e414cfa47cefb9f1b376e
orab <orabfy@gmail.com>
2014-11-05 21:21:15 +0800

    add Chapter 3 solutions.

--- Chapter2/ch2_5.scm
+++ Chapter2/ch2_5.scm
@@ -121,3 +121,19 @@
   ((get 'make-from-real-imag 'complex) x y))
 (define (make-complex-from-mag-ang r a)
   ((get 'make-from-mag-ang 'complex) r a))
+
+;; polynomial
+(define (add-poly p1 p2)
+  (if (same-variable? (variable p1) (variable p2))
+      (make-poly (variable p1)
+                 (add-terms (term-list p1)
+                            (term-list p2)))
+      (error "Ploys not in same var -- ADD-POLY"
+             (list p1 p2))))
+(define (mul-poly p1 p2)
+  (if (same-variable? (variable p1) (variable p2))
+      (make-poly (variable p1)
+                 (mul-terms (term-list p1)
+                            (term-list p2)))
+      (error "Polys not in same var -- MUL-POLY"
+             (list p1 p2))))
