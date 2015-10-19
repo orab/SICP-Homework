086bdca783efef2aef1bfd5f25484ad963a88f61
orab <orabfy@gmail.com>
2015-10-19 20:39:44 +0800

    review Chapter 2.4

---
+++ Chapter2/binary_tree_set.rkt
@@ -0,0 +1,29 @@
+#lang racket
+
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((equal? x (car set)) #t)
+        (else (element-of-set? x (cdr set)))))
+
+;;(define (union-set set1 set2)
+;;  (cond ((null? set1) set2)
+;;        ((null? set2) set1)
+;;        ((not (element-of-set? (car set1) set2))
+;;         (cons (car set1) set2))
+;;        (else (union-set (cdr set1) set2))))
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else
+         (let ((x1 (car set1)) (x2 (car set2)))
+           (cond ((= x1 x2)
+                  (cons x1
+                        (union-set (cdr set1)
+                                   (cdr set2))))
+                 ((< x1 x2)
+                  (cons x1
+                        (union-set (cdr set1) set2)))
+                 ((> x1 x2)
+                  (cons x2
+                        (union-set set1 (cdr set2)))))))))
